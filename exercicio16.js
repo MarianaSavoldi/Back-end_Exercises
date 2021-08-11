d9966d9cbcbb0ffb1de47706e3865d732ed4625f
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-11 00:01:19 -0300

    Exercicio 17 completo e correçao do 16

--- exercicio16.js
+++ exercicio16.js
@@ -1,4 +1,4 @@
 // { Exercício 16: Retorne o total de documentos que não contêm o campo race. }
 
 // use class;
-db.superheroes.find({ "race": { $exists: false } });
+db.superheroes.find({ "race": { $exists: false } }).count();
