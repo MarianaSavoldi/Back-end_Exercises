86b30bb3800d76c49722644eb41da8ac1e2a5df0
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-12 21:13:23 -0300

    Finalizando o dia 23.2

---
+++ dia_23.2/exercicio10.js
@@ -0,0 +1,5 @@
+// { Exercício 10: Retorne o total de super-heróis com
+// cabelos pretos ou carecas ( "No Hair" ). }
+
+//use class;
+db.superheroes.find({ "aspects.hairColor": { $in: [ "Black", "No Hair" ] } }).count();
---
+++ dia_23.2/exercicio12.js
@@ -0,0 +1,5 @@
+// { Exercício 12: Utilizando o operador $not , retorne o total
+// de super-heróis que não tenham mais de 1.80m de altura. }
+
+// use class;
+db.superheroes.find({ "aspects.height": { $not: { $gt: 180 } } }).count();
