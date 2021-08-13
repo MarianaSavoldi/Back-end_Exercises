940d2917bb13786d54394810b010240232e8f16c
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-13 18:46:04 -0300

    Exercício 7 concluído

---
+++ dia_24.1/exercicio7.js
@@ -0,0 +1,9 @@
+// Exercício 7 : Renomeie o campo budget para estimatedBudget do filme Batman .
+
+// use class;
+db.movies.updateOne(
+  { title: "Batman" },
+  { $rename: {
+    "budget": "estimatedBudget"
+  }}
+);
