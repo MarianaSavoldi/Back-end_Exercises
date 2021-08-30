25e29a0e9bc6e50a728caa6f09416d183c8a787a
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-30 17:53:09 -0300

    Ordenando os passo da criação

---
+++ dia_26.4/refazendo_a_aula/index.js
@@ -0,0 +1,26 @@
+// Passo 1: Importar o express
+const express = require('express');
+
+// Passo 2: "Criar" a aplicação
+const app = express();
+
+// Passo 3: Criando a API
+const books = [
+  { id: 1, title: 'The Lord of Rings', author: 'J.R.R. Tolkien' },
+  { id: 2, title: 'Dune', author: 'Frank Herbert' },
+  { id: 3, title: 'Foundation', author: 'Isaac Asimov' },
+  { id: 4, title: 'The Man in The High Castle', author: 'Philip K Dick' },
+  { id: 5, title: '1984', author: 'George Orwell' },
+  { id: 6, title: 'Brave New World', author: 'Aldous Huxley' }
+];
+
+// Passo 5: Criar os gets
+
+// Passo 6: Criar os posts
+
+// Passo 7: Criar os puts
+
+// Passo 8: Criar o delete
+
+// Passo 4: "Abrir a porta" em que a aplicação vai rodar
+app.listen(3000, () => console.log('Aplicação rodando na porta 3000'));
